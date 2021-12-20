009dbbd2a72fe59def9f3b141a6c101227d7ea0d
Flo-314 <julianflo1916@gmail.com>
2021-12-19 23:04:17 -0300

    .

--- src/Components/header.js
+++ src/Components/header.js
@@ -9,7 +9,7 @@ const   Header = ({cart}) =>  {
             </div>
             <li >
           <Link to="shop/checkout">
-         <img src="https://cdn-icons.flaticon.com/png/512/2838/premium/2838895.png?token=exp=1639964878~hmac=574b2707796c817adf8061ef287a72c1" alt="shopping cart"></img>
+         <img src="https://previews.123rf.com/images/vectorlightstudio/vectorlightstudio2001/vectorlightstudio200100102/144161554-shopping-cart-icon.jpg" alt="shopping cart"></img>
               Checkout({cart.length})</Link>
           
         </li>
